d2c0029141a22b333e9018d63be160cf95351709
maryamsp12 <maryamsp12@gmail.com>
2020-08-07 14:10:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React-Redux shopping cart</a>
+      </header>
+      <main>
+        Our products hh
+      </main>
+      <footer>
+        All rights are reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
