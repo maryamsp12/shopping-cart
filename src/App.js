38d9c416cf03890f4a6e92948e1ca18ce582160d
maryamsp12 <maryamsp12@gmail.com>
2020-08-09 00:33:58 +0300

    create product components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import data from "./data.json";
+import Products from "./components/products";
 function App() {
+
+  const [products, setProducts] = React.useState(data);
+  const [size, setSize] = React.useState("");
+  const [sort, setSort] = React.useState("");
+
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">React-Redux shopping cart</a>
       </header>
       <main>
-        Our products hh
+        <div className="content">
+          <div className="main">
+            <Products prod={products} />
+          </div>
+          <div className="sidebar">
+            cartItems
+          </div>
+        </div>
       </main>
       <footer>
         All rights are reserved
